93b6b99145c395c7149db36c7a2f7729e7560881
cs <clensesanthosh2017@gmail.com>
2023-04-01 20:34:09 +0530

    Foregin key declared(Editted)

--- blah.sql
+++ blah.sql
@@ -1,8 +1,38 @@
-CREATE TABLE accounts (Account_ID INT NOT NULL AUTO_INCREMENT, Account_Name VARCHAR(60) NOT NULL, Account_Description VARCHAR(90), PRIMARY KEY (Account_ID));
-CREATE TABLE users (User_ID INT NOT NULL AUTO_INCREMENT, User_Name VARCHAR(50) NOT NULL, User_Email VARCHAR(100) NOT NULL, Account_ID INT NOT NULL, PRIMARY KEY (User_ID));
-CREATE TABLE passwords (Password_ID INT NOT NULL AUTO_INCREMENT, Password_Hash VARCHAR(255) NOT NULL, Password_Salt VARCHAR(255) NOT NULL, User_ID INT NOT NULL, Primary key (Password_ID));
-CREATE INDEX user_account_id_fk_index ON "users" (Account_ID);
-CREATE INDEX passwrods_user_id_fk_index ON "passwords" (User_ID);
+DROP DATABASE IF EXISTS password_manager;
+
+CREATE DATABASE password_manager;
+
+\c password_manager
+
+CREATE TABLE IF NOT EXISTS accounts
+  (
+     account_id          BIGSERIAL NOT NULL,
+     account_name        VARCHAR(60) NOT NULL,
+     account_description VARCHAR(90),
+     PRIMARY KEY (account_id)
+  );
+
+CREATE TABLE IF NOT EXISTS users
+  (
+     user_id    BIGSERIAL NOT NULL,
+     user_name  VARCHAR(50) NOT NULL,
+     user_email VARCHAR(100) NOT NULL,
+     account_id INT NOT NULL,
+     PRIMARY KEY (user_id)
+  );
+
+CREATE TABLE IF NOT EXISTS passwords
+  (
+     password_id   BIGSERIAL NOT NULL,
+     password_hash VARCHAR(255) NOT NULL,
+     password_salt VARCHAR(255) NOT NULL,
+     user_id       INT NOT NULL,
+     PRIMARY KEY (password_id)
+  );
+
+CREATE INDEX user_account_id_fk_index ON "users" (account_id);
+
+CREATE INDEX passwrods_user_id_fk_index ON "passwords" (user_id);
 ALTER TABLE users
 ADD CONSTRAINT fk_Account_ID
 FOREIGN KEY (Account_ID)
